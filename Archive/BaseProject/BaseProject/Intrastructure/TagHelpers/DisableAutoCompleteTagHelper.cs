39d70d9f7b50b9f0c8b5b0ee127f874cd550cfe6
Daniel Ferguson <dan.ferguson@outlook.com>
2018-07-28 13:50:25 -0500

    Add Auto Complete TagHelper

---
+++ Archive/BaseProject/BaseProject/Intrastructure/TagHelpers/DisableAutoCompleteTagHelper.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BaseProject.Intrastructure.TagHelpers
+{
+    [HtmlTargetElement("input", Attributes = AutocompleteOff, TagStructure = TagStructure.NormalOrSelfClosing)]
+    public class DisableAutoCompleteTagHelper : TagHelper
+    {
+        private const string AutocompleteOff = "autocomplete-off";
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.Attributes.Remove(context.AllAttributes.First(a => a.Name == AutocompleteOff));
+            output.Attributes.Add(new TagHelperAttribute("autocomplete", "off"));
+        }
+    }
+}
