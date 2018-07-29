45316269297c40c7d16985dd287c965cd9cb45fe
Daniel Ferguson <dan.ferguson@outlook.com>
2018-07-28 20:03:57 -0500

    Add a web request class for task lesson

---
+++ Lesson03/Delegates/Delagates/AppTask.cs
@@ -0,0 +1,12 @@
+﻿namespace Delagates
+{
+    public class AppTask
+    {
+        public void Run()
+        {
+            var connector = new GitHubConnector("daniefer");
+            var info = connector.GetFormattedUserInfoAsync().Result;
+            System.Console.WriteLine(info);
+        }
+    }
+}
---
+++ Lesson03/Delegates/Delagates/GitHubConnector.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Delagates
+{
+    public class GitHubConnector
+    {
+        private readonly HttpClient _httpClient;
+
+        public GitHubConnector(string userName)
+        {
+            var accessToken = Environment.GetEnvironmentVariable("MyACAGitHubToken", EnvironmentVariableTarget.User);
+            _httpClient = new HttpClient();
+            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
+            _httpClient.DefaultRequestHeaders.UserAgent.Add(new ProductInfoHeaderValue(new ProductHeaderValue("ACA-NET-Core")));
+            _httpClient.BaseAddress = new Uri($"https://api.github.com/users/{userName}");
+        }
+
+        public async Task<string> GetUserInfoAsync()
+        {
+            var response = await _httpClient.GetAsync("").ConfigureAwait(false);
+            return await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+        }
+
+        public async Task<string> GetFormattedUserInfoAsync()
+        {
+            var body = await GetUserInfoAsync().ConfigureAwait(false);
+            var token = JsonConvert.DeserializeObject<JToken>(body);
+            return token.ToString(Formatting.Indented);
+        }
+    }
+}
