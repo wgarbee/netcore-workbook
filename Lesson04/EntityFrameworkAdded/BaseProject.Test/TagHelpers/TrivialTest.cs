dddefb0a7f9fa3a8f53a367b6439d327da5bae32
Daniel Ferguson <dan.ferguson@outlook.com>
2018-08-16 16:22:01 -0500

    Restructure Lesson 04

---
+++ Lesson04/EntityFrameworkAdded/BaseProject.Test/TagHelpers/TrivialTest.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace BaseProject.Test.TagHelpers
+{
+    public class TrivialTest
+    {
+        [Fact]
+        public void TrivialTestRuns()
+        {
+
+        }
+    }
+}
