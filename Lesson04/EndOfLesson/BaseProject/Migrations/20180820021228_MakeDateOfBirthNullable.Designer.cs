a8da61c7c3a2f766817dff090d65934b158fa6d3
Daniel Ferguson <dan.ferguson@outlook.com>
2018-08-19 21:18:38 -0500

    Convert User DateOfBirth to Nullable

--- Lesson04/EndOfLesson/BaseProject/Migrations/20180820021228_MakeDateOfBirthNullable.Designer.cs
+++ Lesson04/EndOfLesson/BaseProject/Migrations/20180820021228_MakeDateOfBirthNullable.Designer.cs
@@ -4,14 +4,16 @@ using BaseProject.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace BaseProject.Migrations
 {
     [DbContext(typeof(ApplicationContext))]
-    partial class ApplicationContextModelSnapshot : ModelSnapshot
+    [Migration("20180820021228_MakeDateOfBirthNullable")]
+    partial class MakeDateOfBirthNullable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
