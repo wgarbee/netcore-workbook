c6846b8969e35472fcc6f9d7b07e99853b51f0c7
Daniel Ferguson <dan.ferguson@outlook.com>
2018-08-19 21:20:19 -0500

    Create post script to migrate not null DateOfBirth field that should be null

---
+++ Lesson04/EndOfLesson/BaseProject/Migrations/20180820021343_MigrateEmptyDateOfBirthToNull.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BaseProject.Migrations
+{
+    public partial class MigrateEmptyDateOfBirthToNull : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"UPDATE [dbo].[Users] SET [DateOfBirth] = NULL WHERE [DateOfBirth] = '1/1/0001 12:00:00 AM'");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
