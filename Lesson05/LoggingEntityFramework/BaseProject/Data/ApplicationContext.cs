0640ce231b976294e4703e043b158336ce894771
Daniel Ferguson <dan.ferguson@outlook.com>
2018-08-21 18:20:41 -0500

    Add Entity Framework Logging folder

---
+++ Lesson05/LoggingEntityFramework/BaseProject/Data/ApplicationContext.cs
@@ -0,0 +1,25 @@
+﻿using BaseProject.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace BaseProject.Data
+{
+    public class ApplicationContext : DbContext
+    {
+        private readonly ILoggerFactory _loggerFactory;
+
+        public ApplicationContext(DbContextOptions<ApplicationContext> options, ILoggerFactory loggerFactory) : base(options)
+        {
+            _loggerFactory = loggerFactory;
+            _loggerFactory.AddDebug();
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+
+            optionsBuilder.UseLoggerFactory(_loggerFactory);
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ Lesson05/LoggingEntityFramework/BaseProject/Migrations/20180820021228_MakeDateOfBirthNullable.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BaseProject.Migrations
+{
+    public partial class MakeDateOfBirthNullable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DateOfBirth",
+                table: "Users",
+                nullable: true,
+                oldClrType: typeof(DateTime));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DateOfBirth",
+                table: "Users",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldNullable: true);
+        }
+    }
+}
