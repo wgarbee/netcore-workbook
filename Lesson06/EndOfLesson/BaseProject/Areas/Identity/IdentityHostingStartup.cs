0d3037f7b12569f2b162a7b95e653de57d94156f
Daniel Ferguson <dan.ferguson@outlook.com>
2018-08-29 18:29:31 -0500

    Add EndOfLesson folder to Lesson 06

---
+++ Lesson06/EndOfLesson/BaseProject/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,26 @@
+﻿using BaseProject.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(BaseProject.Areas.Identity.IdentityHostingStartup))]
+namespace BaseProject.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) =>
+            {
+                services.AddDbContext<MyIdentityContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("BaseProject")));
+
+                services.AddDefaultIdentity<IdentityUser>()
+                    .AddEntityFrameworkStores<MyIdentityContext>();
+            });
+        }
+    }
+}
---
+++ Lesson06/EndOfLesson/BaseProject/Startup.cs
@@ -0,0 +1,68 @@
+﻿using BaseProject.Data;
+using BaseProject.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BaseProject
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddAuthentication();
+
+            // Comment out if you do not have a local Sql Server installed
+            services.AddDbContext<ApplicationContext>(opts => opts.UseSqlServer(Configuration.GetConnectionString("BaseProject")));
+            // Uncomment if you do not have a local Sql Server installed
+            //services.AddDbContext<ApplicationContext>(opts => opts.UseSqlServer(Configuration.GetConnectionString("BaseProjectHosted")));
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            EnsureDatabaseUpdated(app);
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+            app.UseAuthentication();
+            app.UseMvc();
+        }
+
+        private void EnsureDatabaseUpdated(IApplicationBuilder app)
+        {
+            var scopeFactory = app.ApplicationServices.GetRequiredService<IServiceScopeFactory>();
+            using (var serviceScope = scopeFactory.CreateScope())
+            {
+                using (var context = serviceScope.ServiceProvider.GetService<ApplicationContext>())
+                {
+                    context.Database.Migrate();
+                }
+
+                using (var context = serviceScope.ServiceProvider.GetService<MyIdentityContext>())
+                {
+                    context.Database.Migrate();
+                }
+            }
+        }
+    }
+}
