b3397d8323efbcc6697a28b0d60cc17b5ff31321
Daniel Ferguson <dan.ferguson@outlook.com>
2018-09-11 18:36:04 -0500

    Add Lesson 07 End of Lesson

---
+++ Lesson07/EndOfLesson/BaseProject/Hubs/ChatHub.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.SignalR;
+
+namespace BaseProject.Hubs
+{
+    [Authorize]
+    public class ChatHub : Hub
+    {
+        public override Task OnConnectedAsync()
+        {
+            return base.OnConnectedAsync();
+        }
+
+        public override Task OnDisconnectedAsync(Exception exception)
+        {
+            return base.OnDisconnectedAsync(exception);
+        }
+
+        public  Task SendMessage(string username, string message)
+        {
+            return Clients.User(username).SendAsync("OnSendMessage", this.Context.User.Identity.Name, message);
+        }
+    }
+}
