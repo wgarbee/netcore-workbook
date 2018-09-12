8bbd75a5725ebaed0c3f5b669eb492530bdb6435
Daniel Ferguson <dan.ferguson@outlook.com>
2018-09-11 19:49:31 -0500

    Add Simple Notification Hub

---
+++ Lesson07/SimpleNotificationHub/BaseProject/Hubs/NotificationHub.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace BaseProject.Hubs
+{
+    public class NotificationHub : Hub
+    {
+        public override Task OnConnectedAsync()
+        {
+            return base.OnConnectedAsync();
+        }
+
+        public override Task OnDisconnectedAsync(Exception exception)
+        {
+            return base.OnDisconnectedAsync(exception);
+        }
+
+        //public async Task SendMessage(string user, string message)
+        //{
+        //    await Clients.All.SendAsync("ReceiveMessage", user, message);
+        //}
+    }
+}
