c17c1314ed4e3782927242d222036b89b1416103
Daniel Ferguson <dan.ferguson@outlook.com>
2018-09-11 20:42:58 -0500

    Add Chat Hub

---
+++ Lesson07/ChatHub/BaseProject/Hubs/ChatHub.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace BaseProject.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public Task SendMessage(string username, string message)
+        {
+            return Clients.All.SendAsync("ReceiveMessage", username, message);
+        }
+    }
+}
---
+++ Lesson07/ChatHub/BaseProject/wwwroot/js/site.js
@@ -0,0 +1,22 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+"use strict";
+(function () {
+
+    const connection = new signalR.HubConnectionBuilder().withUrl("/NotificationHub").build();
+
+    let issueCount = 0;
+    const link = $("#notification-count").parent().attr("href");
+    connection.on("OnIssueCreate", function (issue) {
+        issueCount += 1;
+        $("#notification-count").text(issueCount);
+        $("#notification-count").parent().attr("href", link + issue.id);
+    });
+
+    connection.start().catch(function (err) {
+        return console.error(err.toString());
+    });
+
+})();
