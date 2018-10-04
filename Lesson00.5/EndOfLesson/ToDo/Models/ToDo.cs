b13e92ed8ad1804baaef6630e3b81beb9aeebb81
Daniel Ferguson <dan.ferguson@outlook.com>
2018-10-04 18:17:27 -0500

    Add lesson 00.5 StartOfLesson and EndOfLesson folders

---
+++ Lesson00.5/EndOfLesson/ToDo/Models/ToDo.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ToDoApp.Models
+{
+    public class ToDo
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        [UIHint("Status")]
+        public Status Status { get; set; }
+    }
+}
---
+++ Lesson00.5/StartOfLesson/ToDo/Controllers/MyController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ToDo.Controllers
+{
+    public class MyController : Controller
+    {
+        public IActionResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            var englishMessage = hour < 12 ? "Good Morning" : "Good Afternoon";
+            ViewBag.EnglishGreeting = englishMessage;
+            var spanishMessage = hour < 12 ? "Buenos Días" : "Buenas Tardes";
+            ViewData["SpanishGreeting"] = spanishMessage;
+            var mandarinMessage = hour < 12 ? "早上好" : "下午好";
+            return View("Greeting", mandarinMessage);
+        }
+    }
+}
