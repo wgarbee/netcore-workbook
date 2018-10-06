11dadf98fa4af25c1c793b741e310bb988a21003
Daniel Ferguson <dan.ferguson@outlook.com>
2018-10-06 13:50:04 -0500

    Add EndOfLesson folder for Lesson02

---
+++ Lesson02/EndOfLesson/TagHelpers/DayOfTheWeekTagHelper.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace TagHelpers
+{
+    /// <summary>
+    /// Renders <see cref="System.DateTime"/> as the day of the week if 
+    /// the date is within 7 days from today. Otherwise, standard 
+    /// datetime format is rendered.
+    /// </summary>
+    [HtmlTargetElement("*", Attributes = "day-of-the-week")]
+    public class DayOfTheWeekTagHelper : TagHelper
+    {
+        private readonly IHtmlHelper _htmlHelper;
+
+        public DayOfTheWeekTagHelper(IHtmlHelper htmlHelper)
+        {
+            _htmlHelper = htmlHelper;
+        }
+
+        /// <summary>
+        /// An expression to be evaluated against the current model.
+        /// </summary>
+        [HtmlAttributeName("asp-for")]
+        public ModelExpression For { get; set; }
+
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            var value = For.Model as DateTime?;
+
+            if (DateTime.Now.AddDays(7) > value)
+            {
+                output.Content.SetContent(value.Value.DayOfWeek.ToString());
+            }
+            else
+            {
+                output.Content.SetContent(string.Format(For.Metadata.DisplayFormatString ?? "{0}", value));
+            }
+        }
+    }
+}
