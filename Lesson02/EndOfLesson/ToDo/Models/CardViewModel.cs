29b7616883d85b3f1d917a236b37cae1cd1e610f
Daniel Ferguson <dan.ferguson@outlook.com>
2018-10-07 13:15:36 -0500

    Add ViewComponents

---
+++ Lesson02/EndOfLesson/ToDo/Models/CardViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace ToDoApp.Models
+{
+    public class CardViewModel
+    {
+        public string Title { get; set; }
+        public string Summary { get; set; }
+        public string Action { get; set; }
+        public int Width { get; set; }
+    }
+}
