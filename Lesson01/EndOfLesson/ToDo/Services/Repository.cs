39480fadd582f393ae40af584565a25ae90e534d
Daniel Ferguson <dan.ferguson@outlook.com>
2018-10-07 14:42:29 -0500

    Add first part of lesson01

---
+++ Lesson01/EndOfLesson/ToDo/Services/Repository.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using ToDoApp.Models;
+
+namespace ToDoApp.Services
+{
+    public class Repository
+    {
+
+        public static Dictionary<int, Status> Statuses = new Dictionary<int, Status>
+        {
+            { 1, new Status { Id = 1, Value = "Not Started" } },
+            { 2, new Status { Id = 2, Value = "In Progress" } },
+            { 3, new Status { Id = 3, Value = "Done" } }
+        };
+
+        public static List<ToDo> ToDos = new List<ToDo>
+        {
+            new ToDo { Id = 1, Title = "My First ToDo", Description = "Get the app working", Status = Statuses[2] }
+        };
+    }
+}
