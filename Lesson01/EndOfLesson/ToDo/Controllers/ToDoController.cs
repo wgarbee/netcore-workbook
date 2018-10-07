a0061dd648f4ac6ce5f2d7c37ac94c7e8659457f
Daniel Ferguson <dan.ferguson@outlook.com>
2018-10-07 14:42:29 -0500

    Add first part of lesson01

---
+++ Lesson01/EndOfLesson/ToDo/Controllers/ToDoController.cs
@@ -0,0 +1,92 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using ToDoApp.Models;
+using ToDoApp.Services;
+
+namespace ToDoApp.Controllers
+{
+    public class ToDoController : Controller
+    {
+
+        // GET: ToDo
+        public ActionResult Index()
+        {
+            return View(Repository.ToDos);
+        }
+
+        // GET: ToDo/Details/5
+        public ActionResult Details(int id)
+        {
+            return View(Repository.ToDos.FirstOrDefault(x => x.Id == id));
+        }
+
+        // GET: ToDo/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ToDo/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(ToDo collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ToDo/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View(Repository.ToDos.FirstOrDefault(x => x.Id == id));
+        }
+
+        // POST: ToDo/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, ToDo collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ToDo/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View(Repository.ToDos.FirstOrDefault(x => x.Id == id));
+        }
+
+        // POST: ToDo/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, ToDo collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
