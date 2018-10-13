7b63b32e5debbf50db90b15d0c8ae07c03cd1ce4
Daniel Ferguson <dan.ferguson@outlook.com>
2018-10-13 17:56:24 -0500

    Lesson 03 EndOfLesson

---
+++ Lesson03/EndOfLesson/TagHelpers/DayOfTheWeekTagHelper.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace TagHelpers
+{
+    /// <summary>
+    /// Renders <see cref="System.DateTime"/> as the day of the week if 
+    /// the date is within 7 days from today. Otherwise, standard 
+    /// datetime format is rendered.
+    /// </summary>
+    [HtmlTargetElement("*", Attributes = "day-of-the-week")]
+    public class DayOfTheWeekTagHelper : TagHelper
+    {
+        /// <summary>
+        /// An expression to be evaluated against the current model.
+        /// </summary>
+        [HtmlAttributeName("asp-for")]
+        public ModelExpression For { get; set; }
+
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            var value = For.Model as DateTime?;
+
+            if (DateTime.Now.AddDays(7) > value)
+            {
+                output.Content.SetContent(value.Value.DayOfWeek.ToString());
+            }
+            else
+            {
+                output.Content.SetContent(string.Format(For.Metadata.DisplayFormatString ?? "{0}", value));
+            }
+        }
+    }
+}
---
+++ Lesson03/EndOfLesson/ToDo/Controllers/ErrorController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ToDoApp.Controllers
+{
+    public class ErrorController : Controller
+    {
+        public IActionResult Index(int? statusCode)
+        {
+            switch (statusCode)
+            {
+                case 404:
+                    return View("Error404");
+                default:
+                    return View();
+            }
+        }
+    }
+}
