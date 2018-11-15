4c6e6162d073dbde7cbc7341a9965f8b8d4a8c84
Daniel Ferguson <dan.ferguson@outlook.com>
2018-11-15 17:28:42 -0600

    Add Tag controller

---
+++ Lesson04/EndOfLesson/ToDo/Models/Tag.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToDoApp.Models
+{
+    public class Tag
+    {
+        public int Id { get; set; }
+
+        public string Value { get; set; }
+
+        public List<ToDo> ToDos { get; set; }
+    }
+}
