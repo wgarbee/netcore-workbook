b77ba98f4cc325e90dea3b9c18771f5e5016a9a9
Daniel Ferguson <dan.ferguson@outlook.com>
2018-11-27 17:01:34 -0600

    Making an Account controller testable

---
+++ Extensions/TestingBrokenDown/BankWithUs/BankWithUs/Data/IBankContext.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using BankWithUs.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+
+namespace BankWithUs.Data
+{
+    public interface IBankContext
+    {
+        IQueryable<Account> Accounts { get; }
+        void AddEntity<TEntity>(TEntity entity) where TEntity : class;
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken = default(CancellationToken));
+        Task<int> SaveChangesAsync(bool acceptAllChangesOnSuccess, CancellationToken cancellationToken = default(CancellationToken));
+        EntityEntry Update(object entity);
+        EntityEntry<TEntity> Update<TEntity>(TEntity entity) where TEntity : class;
+        void Remove(Account account);
+    }
+}
