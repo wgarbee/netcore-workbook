817ec5059860861f0b37fd6d1f5c8a847abf7a6f
wgarbee <wgarbee@gmail.com>
2019-01-16 22:39:36 -0600

    pushing lssn.5 commit

---
+++ Lesson00.5/StartOfLesson/ToDo/Models/Status.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToDoApp.Models
+{
+    public class Status
+    {
+        public int ID { get; set; }
+
+        public string Value { get; set; }
+    }
+}
---
+++ Lesson00.5/StartOfLesson/ToDo/Models/ToDo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToDoApp.Models
+{
+    public class ToDo
+    {
+        public int ID { get; set; }
+
+        public string Description { get; set; }
+
+        [UIHint("Status")]
+        public Status Status { get; set; }
+    }
+}
